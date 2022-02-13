9a74dbcd507b70bdd899741974acbe0f9ae8f7a2
Ferdaouss Samhi <96252799+Frdsmh@users.noreply.github.com>
2022-02-13 16:20:50 +0100

    Create table.sql

---
+++ table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE etudiant(
+    id int(11) AUTO_INCREMENT PRIMARY KEY,
+    nomcomplet varchar(255),
+    email varchar(255),
+    MotPass varchar(255),
+    CNE varchar(255),
+    imguser varchar(255)
+);
